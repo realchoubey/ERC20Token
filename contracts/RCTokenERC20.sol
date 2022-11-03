ec1a5b2d0cbaf787b02215e8bb8706ce9b7fcc4d
Vikash Choubey <5386997+realchoubey@users.noreply.github.com>
2022-11-03 16:49:04 +0530

    Real Choubey token v3

--- contracts/RCTokenERC20.sol
+++ contracts/RCTokenERC20.sol
@@ -5,37 +5,30 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RCTokenV2 is ERC20 {
     address public minter;
-    mapping (address => uint) public balances;
-    event Sent(address from, address to, uint amount);
 
-    constructor() ERC20("RealChoubeyToken", "RCT") {
+    constructor() ERC20("RealChoubeyTokenV3", "RCT") {
         _mint(msg.sender, 10000 * (10 ** decimals()));
+        minter = msg.sender;
     }
-
-    function symbol() public view virtual override returns (string memory) {
-        return "RCT";
-    }
-
-    function name() public view virtual override returns (string memory) {
-        return "RealChoubeyTokenV2";
-    }
-
 
     function mint(address receiver, uint amount) public {
-        if(msg.sender != minter) return;
-        balances[receiver] += amount;
-        emit Transfer(address(0), msg.sender, amount);
+        require(msg.sender == minter, "ERC20: Incorrect minter");
+        _mint(receiver, amount);
     }
 
-    function balanceOf(address account) public view virtual override returns (uint) {
-        return balances[account];
+    function transfer(address to, uint amount) public virtual override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 
-    function transfer(address to, uint amount) public virtual override returns (bool){
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-        emit Transfer(msg.sender, to, amount);
+    function burn(uint amount) public virtual returns (bool) {
+        require(msg.sender == minter, "ERC20: Incorrect minter");
+        _burn(msg.sender, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) public virtual override returns (bool) {
+        _approve(msg.sender, spender, amount);
         return true;
     }
 }
